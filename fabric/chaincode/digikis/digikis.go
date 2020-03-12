f8562ce2c67325fb16d4c9a5979477420f8f4043
Danyal Ahmad <danyal20098@gmail.com>
2020-03-11 23:30:32 -0700

    initial commit

---
+++ fabric/chaincode/digikis/digikis.go
@@ -0,0 +1,631 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	//"strings"
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/hyperledger/fabric/protos/peer"
+)
+
+//SmartContract is the data structure which represents this contract and on which  various contract lifecycle functions are attached
+type SmartContract struct {
+}
+
+type Bootdevice struct {
+	ObjectType string    `json:"Type"`
+	ID         string    `json:"id"`
+	DeviceID   string    `json:"deviceID"`
+	timestamp  time.Time `json:"timestamp"`
+}
+
+type Heartrate struct {
+	ObjectType string `json:"Type"`
+	ID         string `json:"id"`
+	DeviceID   string `json:"deviceID"`
+	Heartrate  string `json:"heartrate"`
+}
+
+type Bloodpressure struct {
+	ObjectType string `json:"Type"`
+	ID         string `json:"id"`
+	DeviceID   string `json:"deviceID"`
+	Systolic   string `json:"systolic"`
+	Diastolic  string `json:"diastolic"`
+	Heartrate  string `json:"heartrate"`
+}
+
+type Bloodoxygen struct {
+	ObjectType string `json:"Type"`
+	ID         string `json:"id"`
+	DeviceID   string `json:"deviceID"`
+	ox         string `json:"ox"`
+}
+
+type ECG struct {
+	ObjectType string `json:"Type"`
+	ID         string `json:"id"`
+	DeviceID   string `json:"deviceID"`
+	Heartrate  string `json:"heartrate"`
+}
+
+type Sportdata struct {
+	ObjectType    string `json:"Type"`
+	ID            string `json:"id"`
+	DeviceID      string `json:"deviceID"`
+	SportStep     string `json:"sportStep"`
+	SportDistance string `json:"sportDistance"`
+	SportCalorie  string `json:"sportCalorie"`
+	SportTime     string `json:"sportTime"`
+}
+
+type Sleepdata struct {
+	ObjectType string `json:"Type"`
+	ID         string `json:"id"`
+	DeviceID   string `json:"deviceID"`
+	SleepTotal string `json:"sleepTotal"`
+	SleepLow   string `json:"sleepLow"`
+	SleepDeep  string `json:"sleepDeep"`
+}
+
+type Geographicposition struct {
+	ObjectType string `json:"Type"`
+	ID         string `json:"id"`
+	DeviceID   string `json:"deviceID"`
+	Longitude  string `json:"longitude"`
+	Latitude   string `json:"latitude"`
+	Lbsinfo    string `json:"lbsinfo"`
+	Wifi       string `json:"wifi"`
+	Mt         string `json:"mt"`
+}
+
+type Alarm struct {
+	ObjectType string `json:"Type"`
+	ID         string `json:"id"`
+	DeviceID   string `json:"deviceID"`
+	Lbsinfo    string `json:"lbsinfo"`
+	Longitude  string `json:"latitude"`
+	Latitude   string `json:"longitude"`
+	Wifi       string `json:"wifi"`
+	Mt         string `json:"mt"`
+	_Type      string `json:"_type"`
+}
+
+func (t *SmartContract) Init(stub shim.ChaincodeStubInterface) peer.Response {
+
+	fmt.Println("Init Firing!")
+	return shim.Success(nil)
+}
+
+func (t *SmartContract) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
+
+	// Retrieve the requested Smart Contract function and arguments
+	function, args := stub.GetFunctionAndParameters()
+	fmt.Println("Chaincode Invoke Is Running " + function)
+
+	if function == "addBootdevice" {
+		return t.addBootdevice(stub, args)
+	}
+	if function == "addHeartrate" {
+		return t.addHeartrate(stub, args)
+	}
+	if function == "addBloodpressure" {
+		return t.addBloodpressure(stub, args)
+	}
+	if function == "addBloodoxygen" {
+		return t.addBloodoxygen(stub, args)
+	}
+	if function == "addECG" {
+		return t.addECG(stub, args)
+	}
+	if function == "addSportdata" {
+		return t.addSportdata(stub, args)
+	}
+	if function == "querySportdata" {
+		return t.querySportdata(stub, args)
+	}
+	if function == "addSleepdata" {
+		return t.addSleepdata(stub, args)
+	}
+	if function == "querySleepdata" {
+		return t.querySleepdata(stub, args)
+	}
+	if function == "addGeographicposition" {
+		return t.addGeographicposition(stub, args)
+	}
+	if function == "addAlarm" {
+		return t.addAlarm(stub, args)
+	}
+	// if function == "queryBloodpressure" {
+	// 	return t.queryBloodpressure(stub,args)
+	// }
+
+	fmt.Println("Invoke did not find specified function " + function)
+	return shim.Error("Invoke did not find specified function " + function)
+}
+
+func (t *SmartContract) addBootdevice(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+
+	var err error
+
+	if len(args) != 3 {
+		return shim.Error("Incorrect Number of Aruments. Expecting 3")
+	}
+
+	id := args[0]
+	deviceID := args[1]
+	timestamp, err1 := time.Parse(time.RFC3339, args[2])
+	if err1 != nil {
+		return shim.Error(err.Error())
+	}
+
+	// ======Check if id Already exists
+	dataAsBytes, err := stub.GetState(id)
+	if err != nil {
+		return shim.Error("Transaction Failed with Error: " + err.Error())
+	} else if dataAsBytes != nil {
+		return shim.Error("The Inserted ID already Exists: " + id)
+	}
+
+	// ===== Create Object and Marshal to JSON
+	objectType := "bootdevice"
+	data := &Bootdevice{objectType, id, deviceID, timestamp}
+	dataJSONasBytes, err := json.Marshal(data)
+
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// ======= Save to State
+	err = stub.PutState(id, dataJSONasBytes)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// ======= Return Success
+	fmt.Println("Successfully Saved Data")
+	return shim.Success(nil)
+}
+
+func (t *SmartContract) addHeartrate(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+
+	var err error
+
+	if len(args) != 3 {
+		return shim.Error("Incorrect Number of Aruments. Expecting 3")
+	}
+
+	id := args[0]
+	deviceID := args[1]
+	heartrate := args[2]
+
+	// ======Check if id Already exists
+	dataAsBytes, err := stub.GetState(id)
+	if err != nil {
+		return shim.Error("Transaction Failed with Error: " + err.Error())
+	} else if dataAsBytes != nil {
+		return shim.Error("The Inserted ID already Exists: " + id)
+	}
+
+	// ===== Create Object and Marshal to JSON
+	objectType := "heartrate"
+	data := &Heartrate{objectType, id, deviceID, heartrate}
+	dataJSONasBytes, err := json.Marshal(data)
+
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// ======= Save to State
+	err = stub.PutState(id, dataJSONasBytes)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// ======= Return Success
+	fmt.Println("Successfully Saved Data")
+	return shim.Success(nil)
+}
+
+func (t *SmartContract) addBloodpressure(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+
+	var err error
+
+	if len(args) != 5 {
+		return shim.Error("Incorrect Number of Aruments. Expecting 5")
+	}
+
+	id := args[0]
+	deviceID := args[1]
+	systolic := args[2]
+	diastolic := args[3]
+	heartrate := args[4]
+
+	// ======Check if id Already exists
+	dataAsBytes, err := stub.GetState(id)
+	if err != nil {
+		return shim.Error("Transaction Failed with Error: " + err.Error())
+	} else if dataAsBytes != nil {
+		return shim.Error("The Inserted ID already Exists: " + id)
+	}
+
+	// ===== Create Object and Marshal to JSON
+	objectType := "bloodpressure"
+	data := &Bloodpressure{objectType, id, deviceID, systolic, diastolic, heartrate}
+	dataJSONasBytes, err := json.Marshal(data)
+
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// ======= Save to State
+	err = stub.PutState(id, dataJSONasBytes)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// ======= Return Success
+	fmt.Println("Successfully Saved Data")
+	return shim.Success(nil)
+}
+
+func (t *SmartContract) addBloodoxygen(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+
+	var err error
+
+	if len(args) != 3 {
+		return shim.Error("Incorrect Number of Aruments. Expecting 3")
+	}
+
+	id := args[0]
+	deviceID := args[1]
+	ox := args[2]
+
+	// ======Check if id Already exists
+	dataAsBytes, err := stub.GetState(id)
+	if err != nil {
+		return shim.Error("Transaction Failed with Error: " + err.Error())
+	} else if dataAsBytes != nil {
+		return shim.Error("The Inserted ID already Exists: " + id)
+	}
+
+	// ===== Create Object and Marshal to JSON
+	objectType := "bloodoxygen"
+	data := &Bloodoxygen{objectType, id, deviceID, ox}
+	dataJSONasBytes, err := json.Marshal(data)
+
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// ======= Save to State
+	err = stub.PutState(id, dataJSONasBytes)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// ======= Return Success
+	fmt.Println("Successfully Saved Data")
+	return shim.Success(nil)
+}
+
+func (t *SmartContract) addECG(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+
+	var err error
+
+	if len(args) != 3 {
+		return shim.Error("Incorrect Number of Aruments. Expecting 3")
+	}
+
+	id := args[0]
+	deviceID := args[1]
+	heartrate := args[2]
+
+	// ======Check if id Already exists
+	dataAsBytes, err := stub.GetState(id)
+	if err != nil {
+		return shim.Error("Transaction Failed with Error: " + err.Error())
+	} else if dataAsBytes != nil {
+		return shim.Error("The Inserted ID already Exists: " + id)
+	}
+
+	// ===== Create Object and Marshal to JSON
+	objectType := "ecg"
+	data := &ECG{objectType, id, deviceID, heartrate}
+	dataJSONasBytes, err := json.Marshal(data)
+
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// ======= Save to State
+	err = stub.PutState(id, dataJSONasBytes)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// ======= Return Success
+	fmt.Println("Successfully Saved Data")
+	return shim.Success(nil)
+}
+
+func (t *SmartContract) addSportdata(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+
+	var err error
+
+	if len(args) != 6 {
+		return shim.Error("Incorrect Number of Aruments. Expecting 6")
+	}
+
+	id := args[0]
+	deviceID := args[1]
+	sportStep := args[2]
+	sportDistance := args[3]
+	sportCalorie := args[4]
+	sportTime := args[5]
+
+	// ======Check if id Already exists
+	dataAsBytes, err := stub.GetState(id)
+	if err != nil {
+		return shim.Error("Transaction Failed with Error: " + err.Error())
+	} else if dataAsBytes != nil {
+		return shim.Error("The Inserted ID already Exists: " + id)
+	}
+
+	// ===== Create Object and Marshal to JSON
+	objectType := "sportdata"
+	data := &Sportdata{objectType, id, deviceID, sportStep, sportDistance, sportCalorie, sportTime}
+	dataJSONasBytes, err := json.Marshal(data)
+
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// ======= Save to State
+	err = stub.PutState(id, dataJSONasBytes)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// ======= Return Success
+	fmt.Println("Successfully Saved Data")
+	return shim.Success(nil)
+}
+
+func (s *SmartContract) querySportdata(APIstub shim.ChaincodeStubInterface, args []string) peer.Response {
+
+	if len(args) < 1 {
+		return shim.Error("Incorrect number of arguments. Expecting 1")
+	}
+
+	deviceID := args[0]
+	queryString := fmt.Sprintf("{\"selector\":{\"Type\":\"sportdata\",\"deviceID\":\"%s\"}}", deviceID)
+
+	queryResults, err := getQueryResultForQueryString(APIstub, queryString)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	return shim.Success(queryResults)
+}
+
+func (s *SmartContract) querySleepdata(APIstub shim.ChaincodeStubInterface, args []string) peer.Response {
+
+	if len(args) < 1 {
+		return shim.Error("Incorrect number of arguments. Expecting 1")
+	}
+
+	deviceID := args[0]
+	queryString := fmt.Sprintf("{\"selector\":{\"Type\":\"sleepdata\",\"deviceID\":\"%s\"}}", deviceID)
+
+	queryResults, err := getQueryResultForQueryString(APIstub, queryString)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	return shim.Success(queryResults)
+}
+
+func (t *SmartContract) addSleepdata(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+
+	var err error
+
+	if len(args) != 5 {
+		return shim.Error("Incorrect Number of Aruments. Expecting 5")
+	}
+
+	id := args[0]
+	deviceID := args[1]
+	sleepTotal := args[2]
+	sleepLow := args[3]
+	sleepDeep := args[4]
+
+	// ======Check if id Already exists
+	dataAsBytes, err := stub.GetState(id)
+	if err != nil {
+		return shim.Error("Transaction Failed with Error: " + err.Error())
+	} else if dataAsBytes != nil {
+		return shim.Error("The Inserted ID already Exists: " + id)
+	}
+
+	// ===== Create Object and Marshal to JSON
+	objectType := "sleepdata"
+	data := &Sleepdata{objectType, id, deviceID, sleepTotal, sleepLow, sleepDeep}
+	dataJSONasBytes, err := json.Marshal(data)
+
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// ======= Save to State
+	err = stub.PutState(id, dataJSONasBytes)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// ======= Return Success
+	fmt.Println("Successfully Saved Data")
+	return shim.Success(nil)
+}
+
+func (t *SmartContract) addGeographicposition(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+
+	var err error
+
+	if len(args) != 7 {
+		return shim.Error("Incorrect Number of Aruments. Expecting 7")
+	}
+
+	id := args[0]
+	deviceID := args[1]
+	longitude := args[2]
+	latitude := args[3]
+	lbsinfo := args[4]
+	wifi := args[5]
+	mt := args[6]
+
+	// ======Check if id Already exists
+	dataAsBytes, err := stub.GetState(id)
+	if err != nil {
+		return shim.Error("Transaction Failed with Error: " + err.Error())
+	} else if dataAsBytes != nil {
+		return shim.Error("The Inserted ID already Exists: " + id)
+	}
+
+	// ===== Create Object and Marshal to JSON
+	objectType := "geographicposition"
+	data := &Geographicposition{objectType, id, deviceID, longitude, latitude, lbsinfo, wifi, mt}
+	dataJSONasBytes, err := json.Marshal(data)
+
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// ======= Save to State
+	err = stub.PutState(id, dataJSONasBytes)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// ======= Return Success
+	fmt.Println("Successfully Saved Data")
+	return shim.Success(nil)
+}
+
+func (t *SmartContract) addAlarm(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+
+	var err error
+
+	if len(args) != 8 {
+		return shim.Error("Incorrect Number of Aruments. Expecting 8")
+	}
+
+	id := args[0]
+	deviceID := args[1]
+	lbsinfo := args[2]
+	latitude := args[3]
+	longitude := args[4]
+	wifi := args[5]
+	mt := args[6]
+	_type := args[7]
+
+	// ======Check if id Already exists
+	dataAsBytes, err := stub.GetState(id)
+	if err != nil {
+		return shim.Error("Transaction Failed with Error: " + err.Error())
+	} else if dataAsBytes != nil {
+		return shim.Error("The Inserted ID already Exists: " + id)
+	}
+
+	// ===== Create Object and Marshal to JSON
+	objectType := "alarm"
+	data := &Alarm{objectType, id, deviceID, lbsinfo, latitude, longitude, wifi, mt, _type}
+	dataJSONasBytes, err := json.Marshal(data)
+
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// ======= Save to State
+	err = stub.PutState(id, dataJSONasBytes)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// ======= Return Success
+	fmt.Println("Successfully Saved Data")
+	return shim.Success(nil)
+}
+
+// =========================================================================================
+// getQueryResultForQueryString executes the passed in query string.
+// Result set is built and returned as a byte array containing the JSON results.
+// =========================================================================================
+func getQueryResultForQueryString(stub shim.ChaincodeStubInterface, queryString string) ([]byte, error) {
+
+	fmt.Printf("- getQueryResultForQueryString queryString:\n%s\n", queryString)
+
+	resultsIterator, err := stub.GetQueryResult(queryString)
+	if err != nil {
+		return nil, err
+	}
+	defer resultsIterator.Close()
+
+	// buffer is a JSON array containing QueryRecords
+	var buffer bytes.Buffer
+	buffer.WriteString("[")
+
+	bArrayMemberAlreadyWritten := false
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+		if err != nil {
+			return nil, err
+		}
+		// Add a comma before array members, suppress it for the first array member
+		if bArrayMemberAlreadyWritten == true {
+			buffer.WriteString(",")
+		}
+		buffer.WriteString("{\"Key\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(queryResponse.Key)
+		buffer.WriteString("\"")
+
+		buffer.WriteString(", \"Record\":")
+		// Record is a JSON object, so we write as-is
+		buffer.WriteString(string(queryResponse.Value))
+		buffer.WriteString("}")
+		bArrayMemberAlreadyWritten = true
+	}
+	buffer.WriteString("]")
+
+	fmt.Printf("- getQueryResultForQueryString queryResult:\n%s\n", buffer.String())
+
+	return buffer.Bytes(), nil
+}
+
+//Main Function starts up the Chaincode
+func main() {
+	err := shim.Start(new(SmartContract))
+	if err != nil {
+		fmt.Printf("Smart Contract could not be run. Error Occured: %s", err)
+	} else {
+		fmt.Println("Smart Contract successfully Initiated")
+	}
+}
+
+// func (t *SmartContract) queryBloodpressure(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+
+// 	if len(args) < 1 {
+// 	   return shim.Error("Incorrect number of arguments. Expecting 1")
+//    }
+
+//    queryString := fmt.Sprintf("{\"selector\":{\"Type\":\"bloodpressure\",\"deviceID\":\"%s\"}}", args[0])
+
+//    queryResults, err := getQueryResultForQueryString(stub, queryString)
+//    if err != nil {
+// 	   return shim.Error(err.Error())
+//    }
+
+//    return shim.Success(queryResults)
+// }
---
+++ node-SDK/app.js
@@ -0,0 +1,280 @@
+'use strict';
+
+//get libraries
+const express = require('express');
+var queue = require('express-queue');
+const bodyParser = require('body-parser');
+const request = require('request');
+const path = require('path');
+const query = require('./blockchain/queryNetwork');
+
+//create express web-app
+const app = express();
+const health = require('./apis/health')
+const utilities = require('./apis/utilities')
+var _time = "T00:00:00Z";
+
+//declare port
+var port = process.env.PORT || 8000;
+if (process.env.VCAP_APPLICATION) {
+  port = process.env.PORT;
+}
+
+// // app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+//Using queue middleware
+app.use(queue({ activeLimit: 30, queuedLimit: -1 }));
+
+//run app on port
+app.listen(port, function () {
+  console.log('app running on port: %d', port);
+});
+
+//-------------------------------------------------------------
+//----------------------  GET API'S    -----------------------
+//-------------------------------------------------------------
+
+app.get('/devices/receivedata',async function (req,res){
+
+  let ts = Math.floor(new Date() / 1000)
+  let id = (ts + (Math.floor(Math.random() * 1000) + 1)).toString();
+
+  if ( req.query.action == 'synctime')
+  {
+    let currentdate = new Date();
+    var request = {
+      chaincodeId: 'digikis',
+      fcn: 'addBootdevice',
+      args: [
+        id,
+        req.query.deviceid,
+        currentdate.toISOString()
+      ]
+    };
+    console.log(request);
+    let response = await  utilities.synctime(request);
+    if(response)
+    {
+      console.log(response);
+      res.status(response.status).send(response.result);
+    }
+    
+  }
+  else if ( req.query.action == 'sensor')
+  {
+    var request = {
+      chaincodeId: 'digikis',
+      fcn: 'addHeartrate',
+      args: [
+        id,
+        req.query.deviceid,
+        req.query.heart ? req.query.heart : ""
+      ]
+    };
+    console.log(request);
+    let response = await health.sensor(request);
+    if(response)
+    {
+      console.log(response);
+      res.status(response.status).send(response.result);
+    }
+  }
+  else if ( req.query.action == 'submitbloodpressure')
+  {
+    var request = {
+      chaincodeId: 'digikis',
+      fcn: 'addBloodpressure',
+      args: [
+        id,
+        req.query.deviceid,
+        req.query.systolic ? req.query.systolic : "",
+        req.query.diastolic ? req.query.diastolic : "",
+        req.query.heartrate ? req.query.heartrate : ""
+      ]
+    };
+    console.log(request);
+    let response = await health.submitbloodpressure(request);
+    if(response)
+    {
+      console.log(response);
+      res.status(response.status).send(response.result);
+    }
+  }
+  else if ( req.query.action == 'submitox')
+  {
+    var request = {
+      chaincodeId: 'digikis',
+      fcn: 'addBloodoxygen',
+      args: [
+        id,
+        req.query.deviceid,
+        req.query.ox  ? req.query.ox : ""
+      ]
+    };
+    console.log(request);
+    let response = await health.submitox(request);
+    if(response)
+    {
+      console.log(response);
+      res.status(response.status).send(response.result);
+    }
+  }
+  else if ( req.query.action == 'submitecg')
+  {
+    var request = {
+      chaincodeId: 'digikis',
+      fcn: 'addECG',
+      args: [
+        id,
+        req.query.deviceid,
+        req.query.heartrate ? req.query.heartrate : ""
+      ]
+    };
+    console.log(request);
+    let response = await health.submitecg(request);
+    if(response)
+    {
+      console.log(response);
+      res.status(response.status).send(response.result);
+    }
+  }
+  else if ( req.query.action == 'submitsport')
+  {
+    var request = {
+      chaincodeId: 'digikis',
+      fcn: 'addSportdata',
+      args: [
+        id,
+        req.query.deviceid,
+        req.query.sportstep ? req.query.sportstep : "",
+        req.query.sportdistance ? req.query.sportdistance : "",
+        req.query.sportcalorie ? req.query.sportcalorie : "",
+        req.query.sporttime ? req.query.sporttime : ""
+      ]
+    };
+    console.log(request);
+    let response = await health.submitsport(request);
+    if(response)
+    {
+      console.log(response);
+      res.status(response.status).send(response.result);
+    }
+  }
+  else if ( req.query.action == 'submitsleep')
+  {
+    var request = {
+      chaincodeId: 'digikis',
+      fcn: 'addSleepdata',
+      args: [
+        id,
+        req.query.deviceid,
+        req.query.sleeptotal ? req.query.sleeptotal : "",
+        req.query.sleeplow ? req.query.sleeplow : "",
+        req.query.sleepdeep ? req.query.sleepdeep : ""
+      ]
+    };
+    console.log(request);
+    let response = await health.submitsleep(request);
+    if(response)
+    {
+      console.log(response);
+      res.status(response.status).send(response.result);
+    }
+  }
+  else if ( req.query.action == 'submitlocation')
+  {
+    var request = {
+      chaincodeId: 'digikis',
+      fcn: 'addGeographicposition',
+      args: [
+        id,
+        req.query.deviceid,
+        req.query.longitude ? req.query.longitude : "",
+        req.query.latitude ? req.query.latitude : "",
+        req.query.lbsinfo ? req.query.lbsinfo : "",
+        req.query.wifi ? req.query.wifi : "",
+        req.query.mt ? req.query.mt : ""
+      ]
+    };
+    console.log(request);
+    let response = await  utilities.submitlocation(request);
+    if(response)
+    {
+      console.log(response);
+      res.status(response.status).send(response.result);
+    }
+  }
+  else if ( req.query.action == 'police')
+  {
+    var request = {
+      chaincodeId: 'digikis',
+      fcn: 'addAlarm',
+      args: [
+        id,
+        req.query.deviceid,
+        req.query.lbsinfo ? req.query.lbsinfo : "",
+        req.query.latitude ? req.query.latitude : "",
+        req.query.longitude ? req.query.longitude : "",
+        req.query.wifi ? req.query.wifi : "",
+        req.query.mt ? req.query.mt : "",
+        req.query.type ? req.query.type : ""
+      ]
+    };
+    console.log(request.args);
+    let response = await  utilities.police(request);
+    if(response)
+    {
+      console.log(response);
+      res.status(response.status).send(response.result);
+    }
+  }
+  else
+  {
+    res.status(500).send("Wrong Input");
+  }
+
+});
+
+
+app.get('/api/sportdata',async function (req,res){
+  const request = {
+    chaincodeId: 'digikis',
+    fcn: 'querySportdata',
+    args: [
+      req.query.deviceid
+    ]
+  };
+  let response = await query.invokeQuery(request)
+  if (response) {
+    if(response.status == 200)
+      res.status(response.status).send(JSON.parse(response.message));
+    else
+      res.status(response.status).send({ message: response.message });
+  }
+});
+
+app.get('/api/sleepdata',async function (req,res){
+  const request = {
+    chaincodeId: 'digikis',
+    fcn: 'querySleepdata',
+    args: [
+      req.query.deviceid
+    ]
+  };
+  let response = await query.invokeQuery(request)
+  if (response) {
+    if(response.status == 200)
+      res.status(response.status).send(JSON.parse(response.message));
+    else
+      res.status(response.status).send({ message: response.message });
+  }
+});
